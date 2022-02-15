acc672ea3f1c3d11ad4dcb9cf1974b763e1bd4e0
jahangir <jahangir@jahangirs-MacBook-Air.local>
2022-02-16 03:03:43 +0600

    app data collection initial commit

--- main.py
+++ main.py
@@ -1,37 +1,61 @@
-import requests
 from bs4 import BeautifulSoup as soup
 
-# my_url = 'https://www.essentracomponents.com/en-us/access-hardware/hinges/leaf-flag-hinges/leaf-flag-fixed-position-hinges'
-my_url = 'https://www.essentracomponents.com/en-us/access-hardware/hinges/leaf-flag-hinges/leaf-flag-torque-hinges'
+INPUT_FILES = {
+    "1": False,
+    "2": False,
+    "3": False,
+    "4": False,
+    "5": False,
+    "6": False,
+    "7": False,
+    "8": False,
+    "9": False,
+    "10": False,
+    "11": False,
+    "12": False,
+    "13": False,
+    "14": False,
+    "15": False,
+    "16": False,
+    "17": False,
+    "18": False,
+    "19": False,
+    "20": False,
+    "22": False,
+    "23": False,
+    "24": False,
+    # "25": False,
+}
+READ_ALL = False
 
-r = requests.get(my_url)
-html_page = r.text
-page_soup = soup(r.text, 'html.parser')
+output_filename = "output_files/slidesho_apps" + ".csv"
+output_file = open(output_filename, "a")
+headers = "title, subtitle, publisher, country, year, last_month_downloads, last_month_revenue\n"
+# output_file.write(headers)
 
-page_containers = page_soup.find_all("div", {"class":"list_item row"})
+def clean_value(value):
+    if not value:
+        return ""
+    return value.text.strip()
 
-filename = page_soup.find('div', {'class':'span9'}).h1.text.strip() + ".csv"
-f = open(filename, "w")
-headers = "product_name, product_short_description, product_image, product_stock_staus, number_of_sub_products\n"
-f.write(headers)
+for file_name, visited in INPUT_FILES.items():
+    if not visited or READ_ALL:
+        input_file = open("input_files/" + file_name + ".html", "r+")
+        file_content = input_file.read()
+        page_soup = soup(file_content, 'html.parser')
+        title = clean_value(page_soup.find("span", {"class":"app-name-wrapper"}))
+        sub_title = clean_value(page_soup.find("h3", {"class":"subtitle-text"}))
+        last_month_downloads = clean_value(page_soup.find("span", {"class":"downloads"}))
+        last_month_revenue = clean_value(page_soup.find("span", {"class": "revenue"}))
+        about_items = page_soup.find("table", {"class":"about-app-table"}).find_all("tr")
+        for item in about_items:
+            if item.find("td", {"class": "name"}).text.strip() == "Support URL:":
+                publisher = clean_value(item.find("td", {"class": "value"}))
+            if item.find("td", {"class": "name"}).text.strip() == "Most Popular Country:":
+                country = clean_value(item.find("td", {"class": "value"}))
+            if item.find("td", {"class": "name"}).text.strip() == "Country Release Date:":
+                release_date = clean_value(item.find("td", {"class": "value"}))
+        input_file.close()
 
-counter = 1
-
-for container in page_containers:
-    product_name = container.find("div", {"class":"span6 prod_info"}).p.text.strip()
-    product_short_description = container.find("div", {"class":"content"}).text.strip()
-    product_image =  container.find("div", {"class":"fullimage"}).img['src']
-    product_stock_staus = container.find("div", {"class":"span2 extro"}).p.text.strip()
-    number_of_sub_products_string = container.find("a", {"class":"itemVarBtn"}).span.text.strip()
-    number_of_sub_products = int(''.join(filter(str.isdigit, str(number_of_sub_products_string))))
-
-    print(counter)
-    print(product_name)
-    print(product_short_description)
-    print(product_image)
-    print(product_stock_staus)
-    print(number_of_sub_products)
-    counter +=1
-
-    f.write(product_name + ", "+ product_short_description +", "+ product_image +", "+ product_stock_staus +", " + str(number_of_sub_products) +"\n")
-f.close()
+    output_file.write(title + ", " + sub_title + ", " + publisher + ", " + country + ", " + release_date +", " + last_month_downloads +", " + last_month_revenue + "\n")
+output_file.close()
